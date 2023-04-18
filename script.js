5b27db9fd45f7fc045772123123b0d9e58601d4c
Lourdes <121977047+LourdesN@users.noreply.github.com>
2023-04-18 14:06:31 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,77 @@
+const createNav = () => {
+    let nav = document.querySelector('.navbar');
+
+    nav.innerHTML = `
+    <div class="nav">
+    <img src="images/logo.jpeg" class="brand-logo" alt="">
+    <div class="nav-items">
+        <div class="search">
+            <input type="text" class="search-box" placeholder="search brand, product">
+            <button class="search-btn">search</button>
+        </div>
+        <script src="https://cdn.lordicon.com/ritcuqlt.js"></script>
+<lord-icon
+src="https://cdn.lordicon.com/bhfjfgqz.json"
+trigger="hover"
+style="width:40px;height:40px">
+</lord-icon>
+        <script src="https://cdn.lordicon.com/ritcuqlt.js"></script>
+<lord-icon
+src="https://cdn.lordicon.com/cllunfud.json"
+trigger="hover"
+colors="outline:#121331,primary:#000000,secondary:#ebe6ef"
+style="width:40px;height:40px">
+</lord-icon>
+    </div>
+</div>
+<ul class="links-container">
+    <li class="link-item"><a href="#" class="link">home</a></li>
+    <li class="link-item"><a href="#" class="link">women</a></li>
+    <li class="link-item"><a href="#" class="link">men</a></li>
+    <li class="link-item"><a href="#" class="link">kids</a></li>
+    <li class="link-item"><a href="#" class="link">accessories</a></li>
+</ul>
+    `;
+}
+
+createNav();
+
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimenstions = item.getBoundingClientRect();
+    let containerWidth = containerDimenstions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
+const productImages = document.querySelectorAll(".product-images img"); // selecting all image thumbs
+const productImageSlide = document.querySelector(".image-slider"); // seclecting image slider element
+
+let activeImageSlide = 0; // default slider image
+
+productImages.forEach((item, i) => { // loopinh through each image thumb
+    item.addEventListener('click', () => { // adding click event to each image thumbnail
+        productImages[activeImageSlide].classList.remove('active'); // removing active class from current image thumb
+        item.classList.add('active'); // adding active class to the current or clicked image thumb
+        productImageSlide.style.backgroundImage = `url('${item.src}')`; // setting up image slider's background image
+        activeImageSlide = i; // updating the image slider variable to track current thumb
+    })
+})
+const sizeBtns = document.querySelectorAll('.size-radio-btn'); // selecting size buttons
+let checkedBtn = 0; // current selected button
+
+sizeBtns.forEach((item, i) => { // looping through each button
+    item.addEventListener('click', () => { // adding click event to each 
+        sizeBtns[checkedBtn].classList.remove('check'); // removing check class from the current button
+        item.classList.add('check'); // adding check class to clicked button
+        checkedBtn = i; // upading the variable
+    })
+})
